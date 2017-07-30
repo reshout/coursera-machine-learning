a9e7b56d7ffe6c2a0e26837e3026ab7313cd736f
reshout <reshout@gmail.com>
2017-07-30 21:54:39 +0900

    ex8 part4 resolved

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -43,16 +43,8 @@ Theta_grad = zeros(size(Theta));
 J = (1 ./ 2) .* sum(sum(R .* (X * Theta' .- Y) .^ 2));
 
 
-
-
-
-
-
-
-
-
-
-
+X_grad = R .* (X * Theta' .- Y) * Theta;
+Theta_grad = (R .* (X * Theta' .- Y))' * X;
 
 
 % =============================================================
